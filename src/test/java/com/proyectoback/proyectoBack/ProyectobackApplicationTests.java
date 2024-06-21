355fd6e962c9de861636abb933013e620bb429bc
DiegoV97 <velazquezdiego1502@gmail.com>
2024-06-21 19:43:36 +0200

    first commit

---
+++ src/test/java/com/proyectoback/proyectoBack/ProyectobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoback.proyectoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
