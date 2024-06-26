c5b4d3fe72254db28d1ee4e4146b7becae8e7861
Maxi <maaxiliria@gmail.com>
2024-06-26 19:48:57 +0200

    login update

--- src/main/java/com/proyectoback/proyectoBack/entitys/Challenge.java
+++ src/main/java/com/proyectoback/proyectoBack/entitys/Challenge.java
@@ -1,12 +1,9 @@
 package com.proyectoback.proyectoBack.entitys;
 
-import org.w3c.dom.Text;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
