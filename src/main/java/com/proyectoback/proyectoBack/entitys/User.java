8ea6ed0151e528d22b33c1fb22316ada3da3cbcd
Pablo Monteserín <pablomonteserin@pablomonteserin.com>
2024-06-26 18:26:32 +0200

    subida

--- src/main/java/com/proyectoback/proyectoBack/entitys/User.java
+++ src/main/java/com/proyectoback/proyectoBack/entitys/User.java
@@ -17,6 +17,9 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 
+ */
 @Entity
 @Table(name = "user")
 @Getter @Setter
@@ -68,6 +71,46 @@ public class User implements UserDetails{
 	@Override
 	public String getPassword() {
 		return password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAccountNonExpired(boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
+	public void setAccountNonLocked(boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+
+	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
 	}
 	
 
