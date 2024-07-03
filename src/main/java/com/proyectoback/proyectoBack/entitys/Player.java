2d31e61f36e8a6b00190e1448832223191e2a55a
Maxi <maaxiliria@gmail.com>
2024-07-03 15:52:57 +0200

    update

--- src/main/java/com/proyectoback/proyectoBack/entitys/Player.java
+++ src/main/java/com/proyectoback/proyectoBack/entitys/Player.java
@@ -4,10 +4,13 @@ package com.proyectoback.proyectoBack.entitys;
 
 
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.proyectoback.proyectoBack.Dto.UserDto;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,9 +21,9 @@ public class Player extends User{
 
 private int points;
 
- @OneToOne (mappedBy = "player")
+ @OneToMany (mappedBy = "player")
  @JsonBackReference
- private Challenge chellenge;
+ private List<Challenge> challenge;
  
  public Player(UserDto user) {
 		this.username = user.getUsername();
