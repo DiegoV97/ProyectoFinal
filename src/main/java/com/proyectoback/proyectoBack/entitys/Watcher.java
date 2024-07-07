f19da33171cfa216c27658d37bea053aa24db399
Maxi <maaxiliria@gmail.com>
2024-07-07 15:00:43 +0200

    update entity watcher

--- src/main/java/com/proyectoback/proyectoBack/entitys/Watcher.java
+++ src/main/java/com/proyectoback/proyectoBack/entitys/Watcher.java
@@ -3,6 +3,8 @@ package com.proyectoback.proyectoBack.entitys;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.proyectoback.proyectoBack.Dto.UserDto;
 
 import jakarta.persistence.Entity;
@@ -13,12 +15,13 @@ import lombok.Getter;
 import lombok.Setter;
 @Entity
 @Getter @Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Watcher extends User{
 	
 	private int proposedChallenge;
-	 @OneToMany (mappedBy = "watcher")
-	 @JsonBackReference
-	 private List<Challenge> challenge;
+	
+	 @OneToMany(mappedBy = "watcher", fetch = FetchType.LAZY)
+	    private List<Challenge> challenges;
 	 
 	 
 	 public Watcher(UserDto user) {
