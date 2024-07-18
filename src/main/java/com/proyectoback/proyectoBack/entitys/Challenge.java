0a593352ff8675237095212bef6563f14e29818c
DiegoV97 <velazquezdiego1502@gmail.com>
2024-07-18 18:07:35 +0200

    diego

--- src/main/java/com/proyectoback/proyectoBack/entitys/Challenge.java
+++ src/main/java/com/proyectoback/proyectoBack/entitys/Challenge.java
@@ -11,6 +11,7 @@ import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Entity
 @Getter
 @Setter
--- src/main/java/com/proyectoback/proyectoBack/repositories/UserRepository.java
+++ src/main/java/com/proyectoback/proyectoBack/repositories/UserRepository.java
@@ -20,4 +20,6 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 	  
 	  @Query("SELECT u FROM Player u ORDER BY u.points DESC")
 	  	List<User> orderByPoints();
+	  
+	  
 }
