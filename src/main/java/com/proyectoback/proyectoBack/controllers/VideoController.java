b8432004e996109022de2509657a0cb25b3e74db
DiegoV97 <velazquezdiego1502@gmail.com>
2024-07-19 18:40:28 +0200

    videoController

--- src/main/java/com/proyectoback/proyectoBack/controllers/VideoController.java
+++ src/main/java/com/proyectoback/proyectoBack/controllers/VideoController.java
@@ -1,23 +1,18 @@
 package com.proyectoback.proyectoBack.controllers;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.proyectoback.proyectoBack.entitys.Video;
 import com.proyectoback.proyectoBack.repositories.VideoRepository;
 
-	@RestController
-	@RequestMapping("/video")
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 	@RequestMapping("/videos")
->>>>>>> 62cae0363d4aad24a2833b24dd93220ed90f063a
+
 	public class VideoController {
 		
 		@Autowired
